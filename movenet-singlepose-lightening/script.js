87b1385ea6f3dfd571fe0bf09e07d34e5c057f7c
Aqib Nawaz <aqibnawazcs@gmail.com>
2024-08-09 14:41:47 +0500

    load raw TFJS movenet Model

--- movenet-singlepose-lightening/script.js
+++ movenet-singlepose-lightening/script.js
@@ -1,10 +1,24 @@
 const MODEL_PATH = "https://tfhub.dev/google/tfjs-model/movenet/singlepose/lightning/4"
 let movenet = undefined
-
+const EXAMPLE_IMG = document.getElementById("exampleImg")
 async function loadAndRunModel() {
   movenet = await tf.loadGraphModel(MODEL_PATH, { fromTFHub: true })
   let exampleTensorInput = tf.zeros([1, 192, 192, 3], 'int32')
-  let tensorOutput = movenet.predict(exampleTensorInput)
+
+  let imageTensor = tf.browser.fromPixels(EXAMPLE_IMG)
+  console.log(imageTensor.shape)
+
+  let cropStartPoint = [15, 179, 0]
+  let croptSize = [345, 345, 3]
+  let cropedTensor = tf.slice(imageTensor, cropStartPoint, croptSize)
+
+  let resizedTensor = tf.image.resizeBilinear(cropedTensor, [192, 192], true).toInt()
+
+
+  console.log(resizedTensor.shape)
+
+
+  let tensorOutput = movenet.predict(tf.expandDims(resizedTensor));
 
   let arrayOutput = await tensorOutput.array()
 
